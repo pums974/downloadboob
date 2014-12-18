aeda116f8993d713bc44f26d31c7b656a397d2dd
Alexandre Poux <work@alexandrepoux.fr>
2014-12-18 10:18:44 +0100

    better output

--- downloadboob.py
+++ downloadboob.py
@@ -37,7 +37,7 @@ from datetime import datetime,timedelta
 from multiprocessing import Pool,Process,Queue,cpu_count
 from Queue import Empty
 
-
+from io import open
 # hack to workaround bash redirection and encoding problem
 import sys
 import codecs
@@ -58,33 +58,38 @@ rx = re.compile('[ \\/\\?\\:\\>\\<\\!\\\\\\*]+', re.UNICODE)
 def removeSpecial(s):
     return rx.sub(' ', '%s' % s)
 
-def write_file(f,string):
-    f.write(string.encode('utf8'))
-
 def check_backend(backend_name):
-    output = check_output(['videoob', "backend","list"],stderr=devnull,shell=False, env={"PYTHONIOENCODING": "UTF-8"}).decode('utf8')
+    # Check if the backend is installed
+    output = check_output(['videoob', "backend","list"],stderr=stderr, env={"PYTHONIOENCODING": "UTF-8"}).decode('utf8')
     list_backend = output.splitlines()[0].split(": ")[1].split(", ")
     if backend_name in list_backend:
+        print('Backend %s found' %  backend_name, file=stdout)
         return True
     else:
-        output = check_output(["videoob","backend","list-modules"],stderr=devnull,shell=False, env={"PYTHONIOENCODING": "UTF-8"}).decode('utf8')
+        print('Backend %s not found' %  backend_name, file=stdout)
+        # Check if the backend is installable
+        output = check_output(["videoob","backend","list-modules"],stderr=stderr, env={"PYTHONIOENCODING": "UTF-8"}).decode('utf8')
         list_backend=[]
         for line in output.splitlines():
           if not matched(line,"Modules list:"):
             list_backend.append(line.split("] ")[1].split(" ")[0])
         if backend_name in list_backend:
-            p=Popen(["videoob","backend","add",backend_name],stderr=devnull,stdout=devnull,shell=False)
-            return p.wait() == 0
+            print('Installing backend %s' %  backend_name, file=stdout)
+            return call(["videoob","backend","add",backend_name],stderr=stderr) == 0
         else:
+            print('Backend %s unknown' %  backend_name, file=stdout)
             return False
 
-DOWNLOAD_DIRECTORY=".files"
-devnull=open('/dev/null', 'w')
-
 def check_exec(executable):
-    if call(['which', executable],stderr=devnull,stdout=devnull,shell=False) != 0:
-        return False
-    return True
+    print('Looking for which %s to use : ' %  executable, file=stdout, end="")
+    try:
+        output = check_output(['which', executable],stderr=stderr2).decode('utf8')
+        print('%s' %  output, file=stdout)
+        return output
+    except CalledProcessError:
+        print('None', file=stdout)
+        return ""
+
 
 def matched(string, regexp):
     if regexp and string:
@@ -115,6 +120,7 @@ class Downloadboob(object):
 
     def is_downloaded(self, video):
         if os.path.isfile(self.get_filename(video)) or os.path.isfile(self.get_filename(video,m3u=True)):
+           print("%s Already downloaded : %s" % (video.id,video.title), file=stdout)
            return True
         else:
            return False
@@ -179,7 +185,7 @@ class Downloadboob(object):
 
     def videoob_get_info(self,video): # THIS IS COSTLY
         try:
-            output = check_output(['videoob', "info",video.id+"@"+self.backend.name],stderr=devnull,shell=False, env={"PYTHONIOENCODING": "UTF-8"}).decode('utf8')
+            output = check_output(['videoob', "info",video.id+"@"+self.backend.name],stderr=stderr, env={"PYTHONIOENCODING": "UTF-8"}).decode('utf8')
             for line in output.splitlines():
                 prefix=line.split(": ")[0]
                 suffix=line[len(prefix)+2:]
@@ -202,30 +208,30 @@ class Downloadboob(object):
             self.backend.fill_video(video, ('ext','title', 'url', 'duration', 'author', 'date', 'description')) # BUGGY FOR ARTE
 
     def purge(self):
-        #remove link if file have been removed
+        #remove link if target have been removed
         if not os.path.isdir(self.links_directory):
             return
         dirList=os.listdir(self.links_directory)
         for local_link_name in dirList:
             link_name = self.links_directory + "/" + local_link_name
             if not self.check_link(link_name):
-                print("Remove %s" % link_name)
+                print("Remove %s" % link_name,file=stdout)
                 os.remove(link_name)
 
     def init_dir(self):
         # create directory
         if not os.path.isdir(self.links_directory):
-            print("  create link directory : %s" % self.links_directory)
+            print("  create link directory : %s" % self.links_directory,file=stdout)
             os.makedirs(self.links_directory)
         if kodi:
             file_name = os.path.join(self.links_directory, 'tvshow.nfo')
             Show_name = self.links_directory.split("/")[-1]
             if not os.path.isfile(file_name):
-                print("  create %s" % file_name)
+                print("  create %s" % file_name,file=stdout)
                 f=open(file_name,'w')
-                write_file(f,"<tvshow>\n")
-                write_file(f,"  <title>"+Show_name+"</title>\n")
-                write_file(f,"</tvshow>\n")
+                f.write("<tvshow>\n")
+                f.write("  <title>"+Show_name+"</title>\n")
+                f.write("</tvshow>\n")
                 f.close()
 
     def do_search(self, pattern=None,pattern_type="search", sortby=CapVideo.SEARCH_RELEVANCE, nsfw=False):
@@ -258,20 +264,19 @@ class Downloadboob(object):
         for video in list_videos:
             if self.is_ok(video,title_regexp,id_regexp,author_regexp,title_exclude):
                 self.videoob_get_info(video)
-                if video and video.url:
+                if not video:
+                    print('Video not found: %s' %  video, file=stderr)
+                elif not video.url:
+                    print('Error: the URL is not available : '+str(video.url), file=stderr)
+                else:
                     if self.is_ok(video,title_regexp,id_regexp,author_regexp,title_exclude):
                         i+=1
                         if not self.is_downloaded(video):
                             videos.append(video)
-#                            print("  %s\n    Description:%s\n    Author:%s\n    Id:%s\n    Duration:%s\n    Date:%s" % (video.title,video.description,video.author, video.id, video.duration, video.date))
-#                        else:
-#                            print("Already downloaded, check %s" % video.id)
+                            print("New Vidéo :  %s\n    Description:%s\n    Author:%s\n    Id:%s\n    Duration:%s\n    Date:%s" 
+                                          % (video.title,video.description,video.author, video.id, video.duration, video.date), file=stdout)
                         if i == max_results:
                             break
-#                elif not video:
-#                    print('Video not found: %s' %  video, file=sys.stderr)
-#                elif not video.url:
-#                    print('Error: the URL is not available : '+str(video.url), file=sys.stderr)
         return videos
 
     def write_m3u(self, video):
@@ -283,55 +288,55 @@ class Downloadboob(object):
             else:
                 dest = self.get_filename(video,m3u=True)
                 Show_name = self.links_directory.split("/")[-1]
-                print("  create %s" % dest)
+                print("  create %s" % dest, file=stdout)
                 f=open(dest, 'w')
-                write_file(f,"#EXTINF: ")
+                f.write("#EXTINF: ")
                 if video.duration:
-                   write_file(f,str(video.duration))
+                   f.write(str(video.duration))
                 else:
-                   write_file(f,str(-1))
-                write_file(f,", "+Show_name+" - "+video.title+"\n")
-                write_file(f,video.url) 
+                   f.write(str(-1))
+                f.write(", "+Show_name+" - "+video.title+"\n")
+                f.write(video.url) 
                 f.close()
                 return 0
 
     def do_download(self, video):
         dest = self.get_filename(video)
         if video.url.startswith('rtmp'):
-            if not check_exec('rtmpdump'):
-                print('I Need rtmpdump')
+            if not exec_rtmpdump:
+                print('I Need rtmpdump', file=stderr)
                 return 1
             args = ['rtmpdump', '-e', '-r', video.url, '-o', dest]
         elif video.url.startswith('mms'):
-            if not check_exec('mimms'):
-                print('I Need mimms')
+            if not exec_mimms:
+                print('I Need mimms', file=stderr)
                 return 1
             args = ['mimms', video.url, dest]
         else:
-            if not check_exec('wget'):
-                if not check_exec('curl'):
-                    print('I Need curl or wget')
+            if not exec_wget:
+                if not exec_curl:
+                    print('I Need curl or wget', file=stderr)
                     return 1
                 else:
                     args = ['curl','-s', video.url, '-o', dest]
             else:
                 args = ['wget','-q', video.url, '-O', dest]
-        return call(args,stderr=devnull,stdout=devnull,shell=False)
+        return call(args,stderr=stderr,stdout=stdout)
 
     def do_conv(self, video):
         dest = self.get_filename(video)
         if matched(str(video.ext),"m3u"):
            video.ext='avi'
            dest = self.get_filename(video)
-           if not check_exec('avconv'):
-              if not check_exec('ffmpeg'):
-                 print('I Need avconv or ffmpeg')
+           if not exec_avconv:
+              if not exec_ffmpeg:
+                 print('I Need avconv or ffmpeg', file=stderr)
                  return 1
               else:
                  args = ['ffmpeg','-i', video.url, '-vcodec','copy','-acodec','copy','-loglevel','error', dest] #"-stat" ,'-threads', '8'
            else:
               args = ['avconv','-i', video.url, '-c','copy', dest]
-           return call(args,stderr=devnull,stdout=devnull,shell=False)
+           return call(args,stderr=stderr,stdout=stdout)
         else:
            return 0
 
@@ -340,7 +345,7 @@ class Downloadboob(object):
         idname = self.get_filename(video, relative=True,m3u=m3u)
         absolute_idname = self.get_filename(video, relative=False,m3u=m3u)
         if not os.path.islink(linkname) and os.path.isfile(absolute_idname):
-#            print("%s -> %s" % (linkname, idname))
+            print("  %s -> %s" % (linkname, idname), file=stdout)
             os.symlink(idname, linkname)
 
     def do_mv(self, video,m3u=False):
@@ -348,7 +353,7 @@ class Downloadboob(object):
         idname = self.get_filename(video, relative=True,m3u=m3u)
         absolute_idname = self.get_filename(video, relative=False,m3u=m3u)
         if not os.path.isfile(linkname) and os.path.isfile(absolute_idname):
-#            print("%s -> %s" % (absolute_idname,linkname))
+            print("  %s => %s" % (absolute_idname,linkname), file=stdout)
             os.rename(absolute_idname, linkname)
             open(absolute_idname, 'w').close() 
 
@@ -357,26 +362,26 @@ class Downloadboob(object):
         nfoname = nfoname+".nfo"
         if not os.path.isfile(nfoname):
             Show_name = self.links_directory.split("/")[-1]
-#            print("  create %s" % nfoname)
+            print("  create %s" % nfoname, file=stdout)
             f=open(nfoname,'w')
-            write_file(f,"<episodedetails>\n")
-            write_file(f,"  <title>"+video.title+"</title>\n")
-            write_file(f,"  <showtitle>"+Show_name+"</showtitle>\n")
-            write_file(f,"  <season>0</season>\n")
-            write_file(f,"  <episode>0</episode>\n")
+            f.write("<episodedetails>\n")
+            f.write("  <title>"+video.title+"</title>\n")
+            f.write("  <showtitle>"+Show_name+"</showtitle>\n")
+            f.write("  <season>0</season>\n")
+            f.write("  <episode>0</episode>\n")
             if video.date:
-                  write_file(f,"  <aired>"+video.date.strftime("%y/%m/%d")+"</aired>\n")
+                  f.write("  <aired>"+video.date.strftime("%y/%m/%d")+"</aired>\n")
             if video.duration:
-                  write_file(f,"  <runtime>"+str(int(video.duration.total_seconds()/60))+"</runtime>\n")
+                  f.write("  <runtime>"+str(int(video.duration.total_seconds()/60))+"</runtime>\n")
             if video.author:
-                  write_file(f,"  <studio>"+video.author+"</studio>\n")
+                  f.write("  <studio>"+video.author+"</studio>\n")
             else:
-                  write_file(f,"  <studio>"+self.backend_name+"</studio>\n")
+                  f.write("  <studio>"+self.backend_name+"</studio>\n")
             if video.description:
-                  write_file(f,"  <plot>"+video.description+"</plot>\n")
-            write_file(f,"  <displayseason />\n")
-            write_file(f,"  <displayepisode />\n")
-            write_file(f,"</episodedetails>\n")
+                  f.write("  <plot>"+video.description+"</plot>\n")
+            f.write("  <displayseason />\n")
+            f.write("  <displayepisode />\n")
+            f.write("</episodedetails>\n")
             f.close()
 
     def download(self, pattern=None, sortby=CapVideo.SEARCH_RELEVANCE, nsfw=False, max_results=50, title_regexp=None, id_regexp=None, \
@@ -408,12 +413,27 @@ class Downloadboob(object):
                     else:
                         self.do_mv(video)           # MOVE FILES FOR A BEAUTIFULL LIBRARY
                         self.write_nfo(video)       # CREATE NFO FILES FOR KODI
-                    print("Downloaded :"+video.title)
+                    print("Downloaded : "+video.title)
                 else:
                     print("Failed download :"+video.title)
 
 
 
+DOWNLOAD_DIRECTORY=".files"
+devnull=open('/dev/null', 'w', encoding="utf-8")
+
+if False: # set it to False to see majors errors
+  stderr=devnull 
+else:
+  stderr=sys.stderr
+if True: # set it to False to see minor errors
+  stderr2=devnull 
+else:
+  stderr2=sys.stderr
+if True: # set it to False to see details
+  stdout=devnull 
+else:
+  stdout=sys.stdout
 
 nproc=cpu_count()
 def do_work(q,r):
@@ -447,25 +467,35 @@ def do_work(q,r):
           if config.has_option(section, "max_results"):
               max_result=config.getint(section, "max_results")
           else:
-              max_result=50
+              max_result=5
           section_links_directory=os.path.join(links_directory, section_sublinks_directory)
 
           downloadboob = Downloadboob(section,backend_name, download_directory, section_links_directory)
           downloadboob.purge()
 
-          print("For backend %s, start search for '%s'" % (backend_name,section))
+          print("For backend %s, start search for '%s'" % (backend_name,section),file=stdout)
           downloadboob.download(pattern, CapVideo.SEARCH_DATE, False, max_result, title_regexp, id_regexp,pattern_type,author_regexp,title_exclude)
-          if matched(pattern_type,"search"):
-              # FIXME (AT LEAST) FOR YOUTUBE
+          if matched(pattern_type,"search"): # FIXME (AT LEAST) FOR YOUTUBE
+              print("For backend %s, start search-bis for '%s'" % (backend_name,section),file=stdout)
               downloadboob.download(pattern, CapVideo.SEARCH_RELEVANCE, False, max_result, title_regexp, id_regexp,pattern_type,author_regexp,title_exclude)
           print("For backend %s, end search for '%s'" % (backend_name,section))
-        else:
-          print(backend_name+" unknown")
     except Empty:
       if q.empty():
         break
 
-if not check_exec('videoob'):
+
+
+exec_videoob=check_exec('videoob')
+exec_wget=check_exec('wget')
+exec_curl=check_exec('curl')
+exec_avconv=check_exec('avconv')
+exec_ffmpeg=check_exec('ffmpeg')
+exec_rtmpdump=check_exec('rtmpdump')
+exec_mimms=check_exec('mimms')
+
+
+if not exec_videoob:
+    print('I need videoob !')
     exit(1)
 
 config = ConfigParser.ConfigParser()
@@ -475,7 +505,7 @@ try:
     links_directory=os.path.expanduser(config.get('main','directory', '.')).decode('utf8')
 except ConfigParser.NoSectionError:
     print("Please create a configuration file (see the README file and the downloadboob.conf example file)")
-    sys.exit(2)
+    exit(2)
 
 download_directory=os.path.join(links_directory, DOWNLOAD_DIRECTORY)
 
@@ -496,8 +526,8 @@ if config.has_option("main", "backend_directory"):
 else:
     backend_directory=os.path.expanduser("~/.local/share/weboob/modules/1.0/")
 
-print("Backends update")
-Popen(["weboob-config","update"]).wait()
+print("Backends update",file=stdout)
+Popen(["weboob-config","update"],stdout=stdout,stderr=stderr).wait()
 
 
 if __name__ == '__main__':
